a041c9b4077cc7307b88144fbb70e35b5eefa02f
matrochanowski <trochanek1@gmail.com>
2025-11-18 13:28:11 +0100

    UI updated

--- simulation/gui/core/window_controller.py
+++ simulation/gui/core/window_controller.py
@@ -11,6 +11,7 @@ class ElevatorSimWindowController(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.setWindowTitle("Elevator Simulation GUI")
 
         # subpages init
         self.main_menu = MainMenuPage(self)
